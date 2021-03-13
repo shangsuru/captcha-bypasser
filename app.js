8f71fc0884be7fed80c0e98b54ea862f4a1988f3
shangsuru <helm.henry99@gmail.com>
2021-03-13 17:26:24 +0100

    Remove demo code

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require("path");
 const captcha = require("nodejs-captcha");
 
-//app.engine("html", require("ejs").renderFile);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -12,25 +11,6 @@ app.get("/", (req, res) => {
     let source = result.image;
     let value = result.value;
     res.render("index", { source, value });
-});
-
-app.get("/captcha", (req, res) => {
-    let result = captcha();
-    let source = result.image;
-    res.end(
-        `
-    <!doctype html>
-    <html>
-        <head>
-            <title>Test Captcha</title>
-        </head>
-        <body>
-        <label>Test image</label>
-        <img src="${source}" />
-        <p>${result.value}</p>
-        </body>
-    </html>`
-    );
 });
 
 app.listen(8000, "127.0.0.1");
